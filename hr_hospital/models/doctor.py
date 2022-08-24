32cf79986fce2745295df9eb729b6ba7f1d52b9a
VoloZH <zhitnyk@gmail.com>
2022-08-24 16:32:28 +0300

    [IMP]

--- hr_hospital/models/doctor.py
+++ hr_hospital/models/doctor.py
@@ -11,10 +11,24 @@ class Doctor(models.Model):
     _inherit = 'hr.hospital.person.mixin'
 
     specialty = fields.Char()
-    intern = fields.Boolean()
+    intern = fields.Boolean(string="Intern",
+                            default=False)
     mentor_id = fields.Many2one(comodel_name='hr.hospital.doctor', string='Mentor for intern', domain="[('intern', '=', False)]")
     intern_ids = fields.One2many(comodel_name='hr.hospital.doctor', inverse_name='mentor_id', string='interns')
     mentor_ids = fields.One2many(comodel_name='hr.hospital.doctor', inverse_name='mentor_id', string='mentors')
     personal_patient_ids = fields.One2many(comodel_name='hr.hospital.patient', inverse_name='personal_doctor_id', string='personal patients')
+    patient_ids = fields.One2many(string="personal patients",
+                                  comodel_name="hr.hospital.patient",
+                                  inverse_name="doctor_id")
 
-
+    def hr_hospital_add_visit_from_doctor_view(self):
+        return {
+            "type": "ir.actions.act_window",
+            "name": "Visit",
+            "res_model": "hr.hospital.visit.to.doctor",
+            "view_type": "form",
+            "view_mode": "form",
+            #  "context": {"default_patient_id": self.ids[0]},
+            'view_id': self.env.ref("hr_hospital.hr_hospital_visit_to_doctor_form").id,
+            "target": "new",
+        }
--- hr_hospital/models/patient.py
+++ hr_hospital/models/patient.py
@@ -19,6 +19,9 @@ class Patient(models.Model):
     personal_doctor_id = fields.Many2one(comodel_name='hr.hospital.doctor')
     history_of_personal_doctor_ids = fields.One2many(comodel_name='hr.hospital.history.of.personal.doctor', inverse_name='patient_id')
     visit_to_doctor_ids = fields.One2many(comodel_name='hr.hospital.visit.to.doctor', inverse_name='patient_id', string='visits history')
+    doctor_id = fields.Many2one(string="Personal doctor",
+                                comodel_name="hr.hospital.doctor",
+                                ondelete="restrict")
  #   patient_id = fields.Many2one(comodel_name='hr.hospital.doctor.multi.wizard')
 
     @api.depends('birthday')
@@ -31,3 +34,35 @@ class Patient(models.Model):
                 rec.age = difference_in_years
             else:
                 rec.age = "Not Provided...."
+
+    def hr_hospital_visit_to_doctor_act_window(self):
+        return {
+            "type": "ir.actions.act_window",
+            "name": "Visits",
+            "res_model": "hr.hospital.visit.to.doctor",
+            "view_type": "form",
+            "view_mode": "tree,form",
+            "domain": [("patient_id", "=", self.id)],
+            "target": "current",
+        }
+
+    def open_list_history_of_personal_doctor(self):
+        self.ensure_one()
+        result = {
+            "type": "ir.actions.act_window",
+            "res_model": "hr.hospital.history.of.personal.doctor",
+            "name": "personal doctor history",
+            'view_mode': 'tree,form',
+        }
+        return result
+
+    def open_list_diagnosis(self):
+        self.ensure_one()
+        result = {
+            "type": "ir.actions.act_window",
+            "res_model": "hr.hospital.diagnosis",
+            "name": "diagnosis",
+            'view_mode': 'tree,form',
+        }
+        return result
+
